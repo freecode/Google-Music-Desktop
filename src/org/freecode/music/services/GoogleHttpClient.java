8c0dd893ba1eb9e355af6da03555124e3b5dee9f
Shivam Mistry <shivam9000@gmail.com>
2013-08-20 18:00:07 +0100

    Adding protobuf files and basic project structure

---
+++ src/org/freecode/music/services/GoogleHttpClient.java
@@ -0,0 +1,15 @@
+package org.freecode.music.services;
+
+/**
+ * User: Shivam
+ * Date: 20/08/13
+ * Time: 16:40
+ */
+public class GoogleHttpClient {
+   	//CONSTANTS
+	private static final String COOKIE_FORMAT = "?u=0&xt=%1$s";
+	private static final String GOOGLE_LOGIN_AUTH_KEY = "Authorization";
+	private static final String GOOGLE_LOGIN_AUTH_VALUE = "GoogleLogin auth=%1$s";
+	private static final String HTTPS_PLAY_MUSIC_LISTEN = "https://play.google.com/music/listen?hl=en&u=0";
+	private static final String HTTPS_PLAY_MUSIC_SERVICES = "https://play.google.com/music/services/";
+}
---
+++ src/org/freecode/music/services/GoogleMusic.java
@@ -0,0 +1,15 @@
+package org.freecode.music.services;
+
+/**
+ * User: Shivam
+ * Date: 20/08/13
+ * Time: 15:33
+ */
+public class GoogleMusic {
+
+	public GoogleMusic() {
+
+	}
+
+
+}
