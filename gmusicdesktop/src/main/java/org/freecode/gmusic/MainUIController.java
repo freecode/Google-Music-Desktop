87764fc2de048fd4110f17526a3d078ff050fc58
Shivam Mistry <shivam9000@gmail.com>
2013-09-13 23:11:01 +0100

    starting to implement seeking

--- gmusicdesktop/src/main/java/org/freecode/gmusic/MainUIController.java
+++ gmusicdesktop/src/main/java/org/freecode/gmusic/MainUIController.java
@@ -1,13 +1,16 @@
 package org.freecode.gmusic;
 
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
+import javafx.scene.input.DragEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,78 +21,112 @@ import javafx.scene.input.MouseEvent;
  */
 public class MainUIController {
 
-    @FXML
-    private ResourceBundle resources;
+	@FXML
+	private ResourceBundle resources;
 
-    @FXML
-    private URL location;
+	@FXML
+	private URL location;
 
-    @FXML
-    private Label album;
+	@FXML
+	private Label album;
 
-    @FXML
-    private ListView<?> albumList;
+	@FXML
+	private ListView<?> albumList;
 
-    @FXML
-    private Label artist;
+	@FXML
+	private Label artist;
 
-    @FXML
-    private ListView<?> artistList;
+	@FXML
+	private ListView<?> artistList;
 
-    @FXML
-    private ListView<?> songList;
+	@FXML
+	private ListView<?> songList;
 
-    @FXML
-    private Label title;
+	@FXML
+	private Label title;
 
+	@FXML //  fx:id="slider"
+	private Slider slider;
 
-    @FXML
-    void albumMouseClicked(MouseEvent event) {
-    }
+	private SongPlayer player;
 
-    @FXML
-    void artistMouseClicked(MouseEvent event) {
-    }
+	public void setSongPlayer(SongPlayer player) {
+		this.player = player;
+	}
 
-    @FXML
-    void songMouseClicked(MouseEvent event) {
-    }
-
-    @FXML
-    void initialize() {
-        assert getAlbum() != null : "fx:id=\"album\" was not injected: check your FXML file 'main-ui.fxml'.";
-        assert getAlbumList() != null : "fx:id=\"albumList\" was not injected: check your FXML file 'main-ui.fxml'.";
-        assert getArtist() != null : "fx:id=\"artist\" was not injected: check your FXML file 'main-ui.fxml'.";
-        assert getArtistList() != null : "fx:id=\"artistList\" was not injected: check your FXML file 'main-ui.fxml'.";
-        assert getSongList() != null : "fx:id=\"songList\" was not injected: check your FXML file 'main-ui.fxml'.";
-        assert getTitle() != null : "fx:id=\"title\" was not injected: check your FXML file 'main-ui.fxml'.";
+	// Handler for Slider[fx:id="slider"] onMouseClicked
+	public void mouseClicked(MouseEvent event) {
+		player.player().seek(new Duration(slider.getValue()));
+	}
 
 
-    }
+
+	@FXML
+	void albumMouseClicked(MouseEvent event) {
+	}
+
+	@FXML
+	void artistMouseClicked(MouseEvent event) {
+	}
+
+	@FXML
+	void songMouseClicked(MouseEvent event) {
+	}
+
+	@FXML
+	void initialize() {
+		assert getAlbum() != null : "fx:id=\"album\" was not injected: check your FXML file 'main-ui.fxml'.";
+		assert getAlbumList() != null : "fx:id=\"albumList\" was not injected: check your FXML file 'main-ui.fxml'.";
+		assert getArtist() != null : "fx:id=\"artist\" was not injected: check your FXML file 'main-ui.fxml'.";
+		assert getArtistList() != null : "fx:id=\"artistList\" was not injected: check your FXML file 'main-ui.fxml'.";
+		assert getSongList() != null : "fx:id=\"songList\" was not injected: check your FXML file 'main-ui.fxml'.";
+		assert getTitle() != null : "fx:id=\"title\" was not injected: check your FXML file 'main-ui.fxml'.";
 
 
-    public Label getAlbum() {
-        return album;
-    }
+	}
 
-    public ListView<?> getAlbumList() {
-        return albumList;
-    }
 
-    public Label getArtist() {
-        return artist;
-    }
+	// Handler for Slider[fx:id="slider"] onDragDetected
+	public void dragDetected(MouseEvent event) {
+		double newPos = slider.getValue();
+		player.player().seek(new Duration(newPos));
+	}
 
-    public ListView<?> getArtistList() {
-        return artistList;
-    }
+	// Handler for Slider[fx:id="slider"] onDragDone
+	public void dragDone(DragEvent event) {
+		System.out.println("drag end");
+		player.setUpdate(false);
+	}
 
-    public ListView<?> getSongList() {
-        return songList;
-    }
+	// Handler for Slider[fx:id="slider"] onDragEntered
+	public void dragStart(DragEvent event) {
+		System.out.println("drag start");
+		player.setUpdate(false);
+	}
 
-    public Label getTitle() {
-        return title;
-    }
+
+	public Label getAlbum() {
+		return album;
+	}
+
+	public ListView<?> getAlbumList() {
+		return albumList;
+	}
+
+	public Label getArtist() {
+		return artist;
+	}
+
+	public ListView<?> getArtistList() {
+		return artistList;
+	}
+
+	public ListView<?> getSongList() {
+		return songList;
+	}
+
+	public Label getTitle() {
+		return title;
+	}
 }
 
--- gmusicdesktop/src/main/java/org/freecode/gmusic/SongPlayer.java
+++ gmusicdesktop/src/main/java/org/freecode/gmusic/SongPlayer.java
@@ -8,13 +8,10 @@ import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.media.Media;
-import javafx.scene.media.MediaBuilder;
 import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
 
-import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URL;
 
 /**
  * Created with IntelliJ IDEA.
@@ -24,49 +21,60 @@ import java.net.URL;
  * To change this template use File | Settings | File Templates.
  */
 public class SongPlayer {
-    private Parent root;
-    private MediaPlayer player;
-    private Song song;
-    private Media media;
+	private Parent root;
+	private MediaPlayer player;
+	private Song song;
+	private Media media;
+	private volatile boolean update = true;
 
-    public SongPlayer(Parent root) {
-        this.root = root;
-    }
+	public SongPlayer(Parent root) {
+		this.root = root;
+	}
 
-    public void init(final Song song, URI songURL) {
-        if(player() != null) {
-            player().stop();
-        }
-        System.out.println(songURL.toString());
-        media = new Media(songURL.toString());
-        player = new MediaPlayer(media);
-        final Slider slider = (Slider) root.lookup("#slider");
-        slider.setMax(song.getDurationMillis());
-        player.currentTimeProperty().addListener(new ChangeListener<Duration>() {
-            public void changed(ObservableValue<? extends Duration> observableValue, Duration duration, Duration nDur) {
-                slider.setValue(nDur.toMillis());
-            }
-        });
-        this.song = song;
-        Platform.runLater(new Runnable() {
-            public void run() {
-                Label artist = (Label) root.lookup("#artist");
-                Label album = (Label) root.lookup("#album");
-                Label title = (Label) root.lookup("#title");
-                artist.setText(song.getArtist());
-                album.setText(song.getAlbum());
-                title.setText(song.getTitle());            }
-        });
+	public void init(final Song song, URI songURL) {
+		if (player() != null) {
+			player().stop();
+		}
+		System.out.println(songURL.toString());
+		media = new Media(songURL.toString());
+		player = new MediaPlayer(media);
+		final Slider slider = (Slider) root.lookup("#slider");
+		slider.setMax(song.getDurationMillis());
+		player.currentTimeProperty().addListener(new ChangeListener<Duration>() {
+			public void changed(ObservableValue<? extends Duration> observableValue, Duration duration, Duration nDur) {
+				if (isUpdate())
+					slider.setValue(nDur.toMillis());
+			}
+		});
+		this.song = song;
+		Platform.runLater(new Runnable() {
+			public void run() {
+				Label artist = (Label) root.lookup("#artist");
+				Label album = (Label) root.lookup("#album");
+				Label title = (Label) root.lookup("#title");
+				artist.setText(song.getArtist());
+				album.setText(song.getAlbum());
+				title.setText(song.getTitle());
+			}
+		});
 
-    }
+	}
 
-    public MediaPlayer player() {
-        return player;
-    }
-
-    public Song song() {
-        return song;
-    }
+	public MediaPlayer player() {
+		return player;
+	}
 
 
+	public Song song() {
+		return song;
+	}
+
+
+	public void setUpdate(boolean update) {
+		this.update = update;
+	}
+
+	public boolean isUpdate() {
+		return update;
+	}
 }
